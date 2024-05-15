dba302d2f446473acba6db17260fc56f0da10d0a
susheelvishwakarma <susheelvishwakarma.india@gmail.com>
2024-05-15 20:12:10 +0530

    Managing Side effect with cleanup function in useEffect

---
+++ React-6 09 may 2024/Assignments/Managing Side Effects with Cleanup Function in useEffect/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import StopWatch from "./components/StopWatch";
+
+const App = () => {
+  const [show, setShow] = useState(true);
+
+  function handleShow() {
+    setShow(!show);
+  }
+
+  return (
+    <div>
+      {/* {show && <StopWatch />} */}
+      {show ? <StopWatch /> : null}
+      <button onClick={handleShow}>TOGGLE BTN</button>
+    </div>
+  );
+};
+
+export default App;
